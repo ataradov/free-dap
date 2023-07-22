61050ae24f93a49251666463af4bd591f6bd7e6d
Heling Yi <heling.yi@gmail.com>
2023-07-21 22:49:32 -0500

    Add jeff probe

--- platform/samd21/hal_config.h
+++ platform/samd21/hal_config.h
@@ -41,6 +41,32 @@
   #define UART_SERCOM_TXPO         0 // PAD[0]
   #define UART_SERCOM_RXPO         1 // PAD[1]
 
+#elif defined(HAL_BOARD_JEFF_PROBE)
+  #define HAL_CONFIG_ENABLE_VCP
+  #define DAP_CONFIG_ENABLE_JTAG
+
+  HAL_GPIO_PIN(SWCLK_TCK,          A, 6)
+  HAL_GPIO_PIN(SWDIO_TMS,          A, 0)
+  HAL_GPIO_PIN(TDI,                A, 16)
+  HAL_GPIO_PIN(TDO,                A, 19)
+  HAL_GPIO_PIN(nRESET,             A, 8)
+
+  HAL_GPIO_PIN(VCP_STATUS,         A, 3);
+  HAL_GPIO_PIN(DAP_STATUS,         A, 6);
+  HAL_GPIO_PIN(BOOT_ENTER,         A, 31);
+
+  HAL_GPIO_PIN(UART_TX,            A, 4);
+  HAL_GPIO_PIN(UART_RX,            A, 7);
+
+  #define UART_SERCOM              SERCOM1
+  #define UART_SERCOM_PMUX         PORT_PMUX_PMUXE_C_Val
+  #define UART_SERCOM_GCLK_ID      SERCOM1_GCLK_ID_CORE
+  #define UART_SERCOM_APBCMASK     PM_APBCMASK_SERCOM1
+  #define UART_SERCOM_IRQ_INDEX    SERCOM1_IRQn
+  #define UART_SERCOM_IRQ_HANDLER  irq_handler_sercom1
+  #define UART_SERCOM_TXPO         1
+  #define UART_SERCOM_RXPO         3
+
 #else
   #error No board defined
 #endif
