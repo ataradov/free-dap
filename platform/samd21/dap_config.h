a2b545bfca2f63b60402d94d351942634ab2a4dd
Heling Yi <heling.yi@gmail.com>
2023-07-22 12:17:23 -0500

    Add LED if HAL_CONFIG_HAS_PWR_STATUS is set Add define for supplying power to target Add option separate nRESET control and nRESET sense Add otpion for nRESET to be active high

--- platform/samd21/dap_config.h
+++ platform/samd21/dap_config.h
@@ -82,7 +82,11 @@ static inline void DAP_CONFIG_nTRST_write(int value)
 //-----------------------------------------------------------------------------
 static inline void DAP_CONFIG_nRESET_write(int value)
 {
+#ifdef DAP_CONFIG_RST_ACTIVE_HIGH
+  HAL_GPIO_nRESET_write(!value);
+#else
   HAL_GPIO_nRESET_write(value);
+#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -126,7 +130,19 @@ static inline int DAP_CONFIG_nTRST_read(void)
 //-----------------------------------------------------------------------------
 static inline int DAP_CONFIG_nRESET_read(void)
 {
+#ifdef DAP_CONFIG_HAS_RST_SENSE
+	#ifdef DAP_CONFIG_RST_ACTIVE_HIGH
+  return ! HAL_GPIO_nRESET_SENSE_read();
+	#else
+  return HAL_GPIO_nRESET_SENSE_read();
+	#endif
+#else
+	#ifdef DAP_CONFIG_RST_ACTIVE_HIGH
+  return ! HAL_GPIO_nRESET_read();
+	#else
   return HAL_GPIO_nRESET_read();
+	#endif
+#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -158,7 +174,17 @@ static inline void DAP_CONFIG_SETUP(void)
 {
   HAL_GPIO_SWCLK_TCK_in();
   HAL_GPIO_SWDIO_TMS_in();
+#ifdef DAP_CONFIG_HAS_TMS_DIR
+  HAL_GPIO_SWDIO_TMS_DIR_out();
+  HAL_GPIO_SWDIO_TMS_DIR_clr();
+#endif
+#ifdef DAP_CONFIG_HAS_RST_SENSE
+  HAL_GPIO_nRESET_SENSE_in();
+  HAL_GPIO_nRESET_out();
+  DAP_CONFIG_nRESET_write(1);
+#else
   HAL_GPIO_nRESET_in();
+#endif
 #ifdef DAP_CONFIG_ENABLE_JTAG
   HAL_GPIO_TDO_in();
   HAL_GPIO_TDI_in();
@@ -170,7 +196,18 @@ static inline void DAP_CONFIG_DISCONNECT(void)
 {
   HAL_GPIO_SWCLK_TCK_in();
   HAL_GPIO_SWDIO_TMS_in();
+#ifdef DAP_CONFIG_HAS_TMS_DIR
+  HAL_GPIO_SWDIO_TMS_DIR_out();
+  HAL_GPIO_SWDIO_TMS_DIR_clr();
+#endif
+
+#ifdef DAP_CONFIG_HAS_RST_SENSE
+  HAL_GPIO_nRESET_SENSE_in();
+  HAL_GPIO_nRESET_out();
+  DAP_CONFIG_nRESET_write(1);
+#else
   HAL_GPIO_nRESET_in();
+#endif
 #ifdef DAP_CONFIG_ENABLE_JTAG
   HAL_GPIO_TDO_in();
   HAL_GPIO_TDI_in();
@@ -186,8 +223,11 @@ static inline void DAP_CONFIG_CONNECT_SWD(void)
   HAL_GPIO_SWCLK_TCK_out();
   HAL_GPIO_SWCLK_TCK_set();
 
+#ifdef DAP_CONFIG_HAS_RST_SENSE
+  HAL_GPIO_nRESET_SENSE_in();
+#endif
   HAL_GPIO_nRESET_out();
-  HAL_GPIO_nRESET_set();
+  DAP_CONFIG_nRESET_write(1);
 
 #ifdef DAP_CONFIG_ENABLE_JTAG
   HAL_GPIO_TDO_in();
@@ -204,8 +244,11 @@ static inline void DAP_CONFIG_CONNECT_JTAG(void)
   HAL_GPIO_SWCLK_TCK_out();
   HAL_GPIO_SWCLK_TCK_set();
 
+#ifdef DAP_CONFIG_HAS_RST_SENSE
+  HAL_GPIO_nRESET_SENSE_in();
+#endif
   HAL_GPIO_nRESET_out();
-  HAL_GPIO_nRESET_set();
+  DAP_CONFIG_nRESET_write(1);
 
 #ifdef DAP_CONFIG_ENABLE_JTAG
   HAL_GPIO_TDO_in();
---
+++ platform/samd21/sys_init.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2016, Alex Taradov <alex@taradov.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*- Includes ----------------------------------------------------------------*/
+#include "samd21.h"
+#include "nvm_data.h"
+#include "hal_config.h"
+
+//-----------------------------------------------------------------------------
+void sys_init(void)
+{
+  uint32_t coarse, fine;
+
+#ifdef HAL_CONFIG_HAS_PWR_STATUS
+  HAL_GPIO_PWR_STATUS_out();
+  HAL_GPIO_PWR_STATUS_set();
+#endif
+
+#ifdef DAP_CONFIG_SUPPLY_PWR
+  HAL_GPIO_SUPPLY_PWR_out();
+  HAL_GPIO_SUPPLY_PWR_set();
+#endif
+
+  SYSCTRL->OSC8M.bit.PRESC = 0;
+
+  SYSCTRL->INTFLAG.reg = SYSCTRL_INTFLAG_BOD33RDY | SYSCTRL_INTFLAG_BOD33DET |
+      SYSCTRL_INTFLAG_DFLLRDY;
+
+  NVMCTRL->CTRLB.bit.RWS = 1;
+
+  coarse = NVM_READ_CAL(NVM_DFLL48M_COARSE_CAL);
+  fine = NVM_READ_CAL(NVM_DFLL48M_FINE_CAL);
+
+  SYSCTRL->DFLLCTRL.reg = 0; // See Errata 9905
+  while (0 == (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY));
+
+  SYSCTRL->DFLLMUL.reg = SYSCTRL_DFLLMUL_MUL(48000);
+  SYSCTRL->DFLLVAL.reg = SYSCTRL_DFLLVAL_COARSE(coarse) | SYSCTRL_DFLLVAL_FINE(fine);
+
+  SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE | SYSCTRL_DFLLCTRL_USBCRM |
+      SYSCTRL_DFLLCTRL_MODE | SYSCTRL_DFLLCTRL_BPLCKC | SYSCTRL_DFLLCTRL_CCDIS |
+      SYSCTRL_DFLLCTRL_STABLE;
+
+  while (0 == (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY));
+
+  GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(0) | GCLK_GENCTRL_SRC(GCLK_SOURCE_DFLL48M) |
+      GCLK_GENCTRL_RUNSTDBY | GCLK_GENCTRL_GENEN;
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+}
+
